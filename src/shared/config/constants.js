ca2f4fe0a8382df7219943cf9e6bf7ba6df0bd26
XeniHong <yui.flaff@mail.ru>
2024-10-10 17:08:06 +0500

    add

---
+++ src/shared/config/constants.js
@@ -0,0 +1,11 @@
+const API_ENDPOINTS = {
+    posts: {
+        news: "newsPosts",
+    },
+}
+
+const API_URL = "api/v1"
+
+const BASE_URL = window.location.origin
+
+export { API_URL, API_ENDPOINTS, BASE_URL }
