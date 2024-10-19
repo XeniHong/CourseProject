f04b913e171017a877dfa86ae197698a9602bb97
XeniHong <yui.flaff@mail.ru>
2024-10-19 16:31:13 +0500

    switchUnivers

---
+++ src/shared/ui/Switch/ui/Switch.js
@@ -0,0 +1,10 @@
+/**
+ * Компонент свитч - кнопки
+ * @return {string}
+ */
+export const Switch = ({ label = "", name = "", extraAttrs = [] } = {}) => {
+  return `<label class="switch">
+              <input type="checkbox"  label ="${label}" class="switch__input visuallyHidden" checked>
+              <span class="switch__slider">${name}</span>
+          </label>`;
+};
